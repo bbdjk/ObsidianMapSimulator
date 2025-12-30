1c84269851d6eda4015f42b26921cd5b6330e620
bbdjk <bbdjk06022@gmail.com>
2025-12-30 14:28:38 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,14 +7,12 @@ const ASSETS = [
   './icon-512.png'
 ];
 
-// 설치 시 캐시 저장
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// 실행 시 캐시에서 로드 (오프라인 지원)
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
